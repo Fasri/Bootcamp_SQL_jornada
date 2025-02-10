b86e76b35bfabb2b8e70f7150346bde85e206be7
Fasri <felipeasr@gmail.com>
2025-02-10 05:33:40 -0300

    Aula 02

---
+++ Bootcamp_SQL/Aula02.sql
@@ -0,0 +1,41 @@
+SELECT * FROM customers
+
+SELECT * FROM categories
+
+SELECT contact_name, city FROM customers
+
+SELECT company_name, address FROM customers
+
+SELECT country FROM customers
+
+SELECT DISTINCT country FROM customers
+
+SELECT DISTINCT contact_name FROM customers
+
+SELECT COUNT(DISTINCT country) FROM customers
+
+SELECT COUNT(DISTINCT contact_name) FROM customers
+
+SELECT * FROM  customers WHERE country = 'Mexico'
+
+SELECT * FROM employees WHERE title = 'Sales Representative'
+
+SELECT * FROM customers WHERE customer_id = 'ANATR'
+
+SELECT * FROM customers WHERE country='Germany' AND city='Berlin'
+
+SELECT * FROM customers WHERE city='Berlin' OR city='Aachen';
+
+SELECT * FROM customers WHERE country<>'Germany'; --todos, menos os registros que tenham Germany
+
+SELECT * FROM customers WHERE country='Germany' AND (city='Berlin' OR city='Aachen');
+
+SELECT * FROM customers WHERE country<>'Germany' AND country<>'USA';
+
+
+
+
+
+
+
+
